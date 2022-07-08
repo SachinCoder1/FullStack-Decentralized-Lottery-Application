14307746e6c50b5bacc76b9f07afa6f3decbb60c
SachinCoder1 <sachinhlo232@gmail.com>
2022-07-08 14:43:10 +0530

    test enterLottery function

--- test/unit/DecentralizedLottery.test.js
+++ test/unit/DecentralizedLottery.test.js
@@ -8,7 +8,7 @@ const {
 if (!developmentChains.includes(network.name)) {
   describe.skip;
 } else {
-  describe("DecentralizedLottery", async () => {
+  describe("DecentralizedLottery", () => {
     let decentralizedLottery,
       vrfCoordinatorV2Mock,
       entranceFee,
@@ -30,6 +30,54 @@ if (!developmentChains.includes(network.name)) {
       entranceFee = await decentralizedLottery.getEntranceFee();
       interval = await decentralizedLottery.getInterval();
     });
+
+    // Checks for the values are assigned correctly in constructor;
+    describe("constructor", () => {
+      it("Check lottery state", async () => {
+        const lotteryState = await decentralizedLottery.getLotteryState();
+        assert.equal(lotteryState.toString(), "0");
+      });
+      it("Check lottery interval", async () => {
+        assert.equal(interval, networkConfig[chainId]["interval"]);
+      });
+    });
+
+    describe("enterLottery", () => {
+      it("Check if it reverts successfully if the amount is less then the entrance fee.", async () => {
+        await expect(decentralizedLottery.enterLottery()).to.be.revertedWith(
+          "Lottery__NotEnoughEthEntered"
+        );
+      });
+
+      it("push the players address if they enter lottery with entrance fee", async () => {
+        await decentralizedLottery.enterLottery({ value: entranceFee });
+        const playerFromContract = await decentralizedLottery.getPlayer(0);
+        assert.equal(playerFromContract, deployer);
+      });
+
+      it("emits the event when entering lottery", async () => {
+        await expect(
+          decentralizedLottery.enterLottery({ value: entranceFee })
+        ).to.emit(decentralizedLottery, "lotteryEnter");
+      });
+
+      it("dosen't allow the lottery to enter when in calculating state", async () => {
+        await decentralizedLottery.enterLottery({ value: entranceFee });
+        await network.provider.send("evm_increaseTime", [
+          interval.toNumber() + 1,
+        ]);
+        await network.provider.send("evm_mine", []);
+        // await network.provider.request({method: "evm_mine", params: []})
+        await decentralizedLottery.performUpkeep([]);
+        await expect(
+          decentralizedLottery.enterLottery({ value: entranceFee })
+        ).to.be.revertedWith("Lottery__NotOpen");
+      });
+    });
+
+    
+
+
 
   });
 }
